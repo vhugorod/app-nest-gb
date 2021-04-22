412c1b65ae37efe9223e518025c1dadd8eee1333
Vitorhr10 <vitorrodrigueshr10@gmail.com>
2021-04-22 17:58:36 -0300

    Short description
    Extended description

---
+++ src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
+import { UserDto } from './dtos/user.dto';
+import { User } from './interfaces/user.interface';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  index(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  @ApiBody({ type: UserDto })
+  create(@Body() user: UserDto): User {
+    return this.usersService.create(user);
+  }
+}
